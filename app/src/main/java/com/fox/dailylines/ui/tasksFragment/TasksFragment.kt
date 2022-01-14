d7b52329b8767220ee4cf45d31cd0bc80f7da594
Moe <Mohammed27Zalzala11@gmail.com>
2022-01-14 19:23:09 +0300

    add navigation host and navgraph component

---
+++ app/src/main/java/com/fox/dailylines/ui/tasksFragment/TasksFragment.kt
@@ -0,0 +1,13 @@
+package com.fox.dailylines.ui.tasksFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class TasksFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/fox/dailylines/ui/timerFragment/TimerFragment.kt
@@ -0,0 +1,12 @@
+package com.fox.dailylines.ui.timerFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class TimerFragment : Fragment() {
+
+}
